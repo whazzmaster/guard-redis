22666f18d0c7e73aa2090d9ad9456ca0116d7e1f
Zachery Moneypenny <whazzmaster@gmail.com>
2014-12-29 12:18:27 -0600

    Updates rake tasks and spec_helper for rspec

--- Rakefile
+++ Rakefile
@@ -1,16 +1,9 @@
 require 'bundler'
+
 Bundler::GemHelper.install_tasks
 
-require 'rspec/core/rake_task'
-RSpec::Core::RakeTask.new(:spec)
-task :default => :spec
-
-def alias_task(tasks)
-    tasks.each do |new_name, old_name|
-        task new_name, [*Rake.application[old_name].arg_names] => [old_name]
-    end
+begin
+  require 'rspec/core/rake_task'
+  RSpec::Core::RakeTask.new(:spec)
+rescue LoadError
 end
-
-alias_task [
-    [:test, :spec]
-]
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,9 @@
 require 'rspec'
-require 'guard/rspec'
-ENV["GUARD_ENV"] = 'test'
+require 'guard/compat/test/helper'
+require 'guard/redis'
 
 RSpec.configure do |config|
-  config.color_enabled = true
-  config.filter_run :focus => true
-  config.run_all_when_everything_filtered = true
-
   config.before(:each) do
-    @fixture_path = Pathname.new(File.expand_path('../fixtures/', __FILE__))
-    @lib_path     = Pathname.new(File.expand_path('../../lib/', __FILE__))
+    allow(Guard::UI).to receive(:info)
   end
-
 end
